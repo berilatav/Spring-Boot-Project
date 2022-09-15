f05c043d52e3ee5877e7feab29d57d75fb275c79
Beril Atav <elifberil1661@gmail.com>
2022-09-15 17:09:39 +0300

    main proje gonderildi

--- src/main/java/com/demo/readingisgood/configuration/EnumMessages.java
+++ src/main/java/com/demo/readingisgood/configuration/EnumMessages.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum EnumMessages {
 
     INTERNAL_SERVER_ERROR(1000, "Internal server error.", HttpStatus.INTERNAL_SERVER_ERROR),
-    BOOK_ATTRIBUTED_SAME(1001, "Book attribute same.", HttpStatus.BAD_REQUEST),
+    BOOK_ATTRIBUTED_SAME(1001, "Book attribute same..", HttpStatus.BAD_REQUEST),
     DUPLICATE_BOOK_ERROR(1002, "Book not able to duplicated.", HttpStatus.BAD_REQUEST);
 
     private int code;
